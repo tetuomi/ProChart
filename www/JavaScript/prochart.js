9ebe1cdda2351dbebe804f60faa8c2b7322a1f3a
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-05-24 17:04:32 +0900

    appear textbox when otheroption chose

--- www/JavaScript/prochart.js
+++ www/JavaScript/prochart.js
@@ -1,4 +1,28 @@
+function pon(text){
+    alert(text);
+};
+function log(text){
+    console.log(text);
+};
+
+
 function appear(){
     var div=document.getElementById("record");
     div.setAttribute("style","display:inline");
-}
+};
+
+function createInput(TARGET, POSITION){
+    var target = document.getElementById(TARGET);
+    var textBox = document.createElement('input');
+    var resisterBox = document.createElement('input');
+  
+    textBox.type = "text";
+    resisterBox.type = "button";
+    resisterBox.id = "resisterBox";
+    resisterBox.value = "完了";
+    
+    target.insertAdjacentElement(POSITION, resisterBox);
+    target.insertAdjacentElement(POSITION, textBox);
+    
+    console.log("create form");
+};
